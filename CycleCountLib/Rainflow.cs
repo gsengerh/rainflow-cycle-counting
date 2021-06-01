23826a0b03c8dfb1fde2ad26475673c971c0cdb8
Carlos Souto <83190503+carlosouto@users.noreply.github.com>
2021-06-01 22:04:48 +0100

    Add files via upload

---
+++ CycleCountLib/Rainflow.cs
@@ -0,0 +1,141 @@
+﻿/* -------------------------------------------------------------------------- */
+/* Rainflow cycle counting algorithm according to:                            */
+/* ASTM E1049-85,                                                             */
+/* Standard Practices for Cycle Counting in Fatigue Analysis,                 */
+/* ASTM International. (https://doi.org/10.1520/E1049-85R17)                  */
+/*                                                                            */
+/* By: Carlos Souto - csouto@fe.up.pt                                         */
+/* -------------------------------------------------------------------------- */
+/* Extracted from ASTM E1049-85 – Rainflow Counting:                          */
+/* Rules for this method are as follows: let X denote range under             */
+/* consideration; Y, previous range adjacent to X; and S, starting point in   */
+/* the history.                                                               */
+/* (1) Read next peak or valley. If out of data, go to Step 6.                */
+/* (2) If there are less than three points, go to Step 1. Form ranges X and Y */
+/*     using the three most recent peaks and valleys that have not been       */
+/*     discarded.                                                             */
+/* (3) Compare the absolute values of ranges X and Y.                         */
+/*     (a) If X < Y, go to Step 1.                                            */
+/*     (b) If X >= Y, go to Step 4.                                           */
+/* (4) If range Y contains the starting point S, go to Step 5; otherwise,     */
+/*     count range Y as one cycle; discard the peak and valley of Y; and go   */
+/*     to Step 2.                                                             */
+/* (5) Count range Y as one-half cycle; discard the first point (peak or      */
+/*     valley) in range Y; move the starting point to the second point in     */
+/*     range Y; and go to Step 2.                                             */
+/* (6) Count each range that has not been previously counted as one-half      */
+/*     cycle.                                                                 */
+/* -------------------------------------------------------------------------- */
+/* License: BSD-3-Clause - https://opensource.org/licenses/BSD-3-Clause       */
+/*                                                                            */
+/* Copyright (c) 2021, Carlos Daniel Santos Souto.                            */
+/* All rights reserved.                                                       */
+/*                                                                            */
+/* Redistribution and use in source and binary forms, with or without         */
+/* modification, are permitted provided that the following conditions are     */
+/* met:                                                                       */
+/*                                                                            */
+/* 1. Redistributions of source code must retain the above copyright notice,  */
+/*    this list of conditions and the following disclaimer.                   */
+/* 2. Redistributions in binary form must reproduce the above copyright       */
+/*    notice, this list of conditions and the following disclaimer in the     */
+/*    documentation and/or other materials provided with the distribution.    */
+/* 3. Neither the name of the copyright holder nor the names of its           */
+/*    contributors may be used to endorse or promote products derived from    */
+/*    this software without specific prior written permission.                */
+/*                                                                            */
+/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS    */
+/* IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,  */
+/* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR     */
+/* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR          */
+/* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,      */
+/* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,        */
+/* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR         */
+/* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF     */
+/* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING       */
+/* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS         */
+/* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.               */
+/* -------------------------------------------------------------------------- */
+
+using Array = System.Array;
+using Math = System.Math;
+
+namespace CycleCountLib
+{
+	/// <summary>
+	/// Static class for the rainflow cycle counting algorithm.
+	/// </summary>
+	public static class Rainflow
+	{
+		/// <summary>
+		/// Rainflow counting algorithm according to ASTM E1049-85.
+		/// </summary>
+		/// <param name="history">The provided stress-time history.</param>
+		/// <returns>The cycle counts returned as a matrix: column 0 are the counts; column 1 are the ranges; column 2 are the mean values.</returns>
+		public static double[][] Execute(double[] history)
+		{
+			double[] extrema = Extrema(history);
+
+			int pidx = -1, eidx = -1, ridx = -1;
+			double xRange, yRange, yMean;
+			int[] points = new int[extrema.Length];
+			double[][] results = new double[extrema.Length][];
+
+			for (int i = 0; i < extrema.Length; i++)
+			{
+				points[++pidx] = ++eidx;
+
+				while (pidx >= 2 && (xRange = Math.Abs(extrema[points[pidx - 1]] - extrema[points[pidx]])) >= (yRange = Math.Abs(extrema[points[pidx - 2]] - extrema[points[pidx - 1]])))
+				{
+					yMean = 0.5 * (extrema[points[pidx - 2]] + extrema[points[pidx - 1]]);
+
+					if (pidx == 2)
+					{
+						results[++ridx] = new double[] { 0.5, yRange, yMean };
+						points[0] = points[1];
+						points[1] = points[2];
+						pidx = 1;
+					}
+					else
+					{
+						results[++ridx] = new double[] { 1.0, yRange, yMean };
+						points[pidx - 2] = points[pidx];
+						pidx -= 2;
+					}
+				}
+			}
+
+			for (int i = 0; i <= pidx - 1; i++)
+			{
+				double range = Math.Abs(extrema[points[i]] - extrema[points[i + 1]]);
+				double mean = 0.5 * (extrema[points[i]] + extrema[points[i + 1]]);
+				results[++ridx] = new double[] { 0.5, range, mean };
+			}
+
+			Array.Resize(ref results, ++ridx);
+			return results;
+		}
+
+		/// <summary>
+		/// Utility function to get the local extrema (maxima and minima) of the provided 
+		/// stress-time history as a sequence of peaks and valleys (bad in-between data 
+		/// points – that are neither peaks nor valleys – are removed).
+		/// </summary>
+		/// <param name="history">The provided stress-time history.</param>
+		/// <returns>The obtained sequence of peaks and valleys (local extrema).</returns>
+		private static double[] Extrema(double[] history)
+		{
+			double[] extrema = new double[history.Length];
+			extrema[0] = history[0];
+
+			int eidx = 0;
+			for (int i = 1; i < history.Length - 1; i++)
+				if ((history[i] > extrema[eidx] && history[i] > history[i + 1]) || (history[i] < extrema[eidx] && history[i] < history[i + 1]))
+					extrema[++eidx] = history[i];
+			extrema[++eidx] = history[history.Length - 1];
+
+			Array.Resize(ref extrema, ++eidx);
+			return extrema;
+		}
+	}
+}
---
+++ CycleCountLib/Testing/main.m
@@ -0,0 +1,68 @@
+% clear workspace
+clear all; close all; clc;
+
+% add .NET assembly
+dll = [pwd, '\..\bin\Release\CycleCountLib.dll'];
+NET.addAssembly(dll);
+
+% number of repetitions/tests
+n = 25;
+
+% timers
+matlab_elapsed = 0;
+csharp_elapsed = 0;
+
+% data sizes to run
+history_size = 1e2:1e3:1e5;
+
+% plot variables
+y_matlab = zeros(length(history_size), 1);
+y_csharp = zeros(length(history_size), 1);
+
+% for each data array size
+for k = 1:1:length(history_size)
+    
+    fprintf('Run %i of %i (data array size = %i)...\n', k, length(history_size), history_size(k));
+    
+    % for each test
+    for i = 1:1:n
+        
+        % generate random stress-time history
+        history = rand(history_size(k), 1);
+
+        % get expected (Requires Signal Processing Toolbox)
+        tic;
+        matlab = rainflow(history);
+        matlab_elapsed = matlab_elapsed + toc;
+        
+        % test
+        tic;
+        csharp = CycleCountLib.Rainflow.Execute(history);
+        csharp_elapsed = csharp_elapsed + toc;
+        
+        % convert data
+        csharp = double(csharp);
+        
+        % check results
+        if matlab(:, 1:3) ~= csharp(:, :)
+            error('Test %i of run %i failed.\n', i, k);
+        else
+            %fprintf('Test %i of run %i passed.\n', i, k);
+        end
+        
+    end
+    
+    y_matlab(k) = 1000*matlab_elapsed/n;
+    y_csharp(k) = 1000*csharp_elapsed/n; 
+    
+end
+
+figure(1); hold on; grid on; legend show;
+set(gca, 'FontSize', 14);
+legend('Location', 'nw');
+xlabel('Stress-time history array size');
+ylabel('Averaged elapsed time [ms]');
+title(sprintf('Averaged elapsed time (%i runs) vs. data size', n));
+subtitle('Lower is better');
+plot(history_size, y_matlab, '-b', 'LineWidth', 2, 'DisplayName', 'MATLAB’s implementation');
+plot(history_size, y_csharp, '-r', 'LineWidth', 2, 'DisplayName', 'This C# code');
